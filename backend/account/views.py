5dbbeb41a4cd62909bf37d043d4ec9c08365e9a0
samimhamza <samim.hamza7@gmail.com>
2022-04-23 08:13:00 +0430

    a

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        try:
+            data = request.data
+            first_name = data["first_name"]
+            last_name = data["last_name"]
+            username = data["username"]
+            password = data["password"]
+            re_password = data["re_password"]
+
+            if password == re_password:
+                if len(password) >= 8:
+                    if not User.objects.filter(username=username).exists():
+                        user = User.objects.create_user(
+                            first_name=first_name,
+                            last_name=last_name,
+                            username=username,
+                            password=password,
+                        )
+                        user.save()
+                        if User.objects.filter(username=username).exists():
+                            return Response(
+                                {"success": "Account Successfully Created"},
+                                status=status.HTTP_201_CREATED,
+                            )
+                        else:
+                            return Response(
+                                {
+                                    "error": "Something went wrong when registering account"
+                                },
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            )
+                    else:
+                        return Response(
+                            {"error": "Username Already Exists"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+                else:
+                    return Response(
+                        {"error": "Password must be at least 8 characters in length"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+            else:
+                return Response(
+                    {"error": "Passwords do not match"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except:
+            return Response(
+                {"error": "Something went wrong while trying to register account"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class LoadUserView(APIView):
+    def get(self, request, format=None):
+        try:
+            user = request.user
+            user = UserSerializer(user)
+            return Response({"user": user.data}, status=status.HTTP_200_OK)
+        except:
+            return Response(
+                {"error": "Something went wrong while loading user"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
